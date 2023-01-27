f548144c30152e155475a17d48879e25818792b5
Eliot <gaillegue.eliot@gmail.com>
2023-01-27 12:04:38 +0000

    adding random stuff6

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
     <h1> Site de Eliot Gaillegue </h1>
 
     <?php echo "hello en php" ?>
+
     <img style="position:absolute" id="myImg2" src="https://media.tenor.com/RQKAyM7ZHA0AAAAM/banana-dance.gif" />
+    <img style="position:absolute" id="myImg3" src="https://media.tenor.com/RQKAyM7ZHA0AAAAM/banana-dance.gif" />
+    <img style="position:absolute" id="myImg4" src="https://media.tenor.com/RQKAyM7ZHA0AAAAM/banana-dance.gif" />
+    <img style="position:absolute" id="myImg5" src="https://media.tenor.com/RQKAyM7ZHA0AAAAM/banana-dance.gif" />
+    <img style="position:absolute" id="myImg6" src="https://media.tenor.com/RQKAyM7ZHA0AAAAM/banana-dance.gif" />
     
     <img id="myImg" src="https://idata.over-blog.com/2/60/21/58/mes-images-8/zidane-pub-98-leader-price.jpg" />
 </div>
--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let cont = document.getElementById("cont")
-let banana = document.getElementById("myImg2")
+let banana1 = document.getElementById("myImg2")
+let banana2 = document.getElementById("myImg3")
+let banana3 = document.getElementById("myImg4")
+let banana4 = document.getElementById("myImg5")
+let banana5 = document.getElementById("myImg6")
+
 
 setInterval(randomColour, 100)
 
-setInterval(bananaMove, 100)
+setInterval(banana1Move, 100)
+
+setInterval(banana2Move, 100)
+
+setInterval(banana3Move, 100)
+
+setInterval(banana4Move, 100)
+
+setInterval(banana5Move, 100)
 
 function randomColour(){
 
@@ -12,17 +25,69 @@ function randomColour(){
 
     cont.style.backgroundColor = "#" + randColor;
 }
-function bananaMove(){
+function banana1Move(){
 
         randplace1 = Math.floor(Math.random() * 99);
         randplace2 = Math.floor(Math.random() * 99);
         randplace3 = Math.floor(Math.random() * 99);
         randplace4 = Math.floor(Math.random() * 99);
 
-        banana.style.top = (randplace1+"%")
-        banana.style.left = (randplace2+"%")
-        banana.style.bottom = (randplace3+"%")
-        banana.style.right = (randplace4+"%")
+        banana1.style.top = (randplace1+"%")
+        banana1.style.left = (randplace2+"%")
+        banana1.style.bottom = (randplace3+"%")
+        banana1.style.right = (randplace4+"%")
+}
+
+function banana2Move(){
+
+    randplace1 = Math.floor(Math.random() * 99);
+    randplace2 = Math.floor(Math.random() * 99);
+    randplace3 = Math.floor(Math.random() * 99);
+    randplace4 = Math.floor(Math.random() * 99);
+
+    banana2.style.top = (randplace1+"%")
+    banana2.style.left = (randplace2+"%")
+    banana2.style.bottom = (randplace3+"%")
+    banana2.style.right = (randplace4+"%")
+}
+
+function banana3Move(){
+
+    randplace1 = Math.floor(Math.random() * 99);
+    randplace2 = Math.floor(Math.random() * 99);
+    randplace3 = Math.floor(Math.random() * 99);
+    randplace4 = Math.floor(Math.random() * 99);
+
+    banana3.style.top = (randplace1+"%")
+    banana3.style.left = (randplace2+"%")
+    banana3.style.bottom = (randplace3+"%")
+    banana3.style.right = (randplace4+"%")
+}
+
+function banana4Move(){
+
+    randplace1 = Math.floor(Math.random() * 99);
+    randplace2 = Math.floor(Math.random() * 99);
+    randplace3 = Math.floor(Math.random() * 99);
+    randplace4 = Math.floor(Math.random() * 99);
+
+    banana4.style.top = (randplace1+"%")
+    banana4.style.left = (randplace2+"%")
+    banana4.style.bottom = (randplace3+"%")
+    banana4.style.right = (randplace4+"%")
+}
+
+function banana5Move(){
+
+    randplace1 = Math.floor(Math.random() * 99);
+    randplace2 = Math.floor(Math.random() * 99);
+    randplace3 = Math.floor(Math.random() * 99);
+    randplace4 = Math.floor(Math.random() * 99);
+
+    banana5.style.top = (randplace1+"%")
+    banana5.style.left = (randplace2+"%")
+    banana5.style.bottom = (randplace3+"%")
+    banana5.style.right = (randplace4+"%")
 }
 
 
