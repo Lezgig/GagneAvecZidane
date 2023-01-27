e089fdac098487dc7edc870598bbbca7a9ce7508
Eliot <gaillegue.eliot@gmail.com>
2023-01-27 11:42:42 +0000

    adding random stuff2

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let cont = document.getElementById("cont")
 
-randColor = Math.floor(Math.random() * 9999);
+setInterval(randomColour, 1000)
 
-cont.style.backgroundColor = "#" + randColor;
+function randomColour(){
+
+    randColor = Math.floor(Math.random() * 9999);
+
+
+    cont.style.backgroundColor = "#" + randColor;
+}
