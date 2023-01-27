ce5a917967115ca17d36e5b31cef7b9bca8614d3
Eliot <gaillegue.eliot@gmail.com>
2023-01-27 11:40:52 +0000

    adding random stuff

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
-<div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+<div id="cont" style="display: flex; flex-direction: column; justify-content: center; align-items: center">
     <h1> Site de Eliot Gaillegue </h1>
 
     <?php echo "hello en php" ?>
-
-    <img src="https://idata.over-blog.com/2/60/21/58/mes-images-8/zidane-pub-98-leader-price.jpg" />
+    <img id="myImg" src="https://media.tenor.com/RQKAyM7ZHA0AAAAM/banana-dance.gif" />
+    
+    <img id="myImg" src="https://idata.over-blog.com/2/60/21/58/mes-images-8/zidane-pub-98-leader-price.jpg" />
 </div>
+<script src= "script.js"></script>
---
+++ script.js
@@ -0,0 +1,5 @@
+let cont = document.getElementById("cont")
+
+randColor = Math.floor(Math.random() * 9999);
+
+cont.style.backgroundColor = "#" + randColor;
