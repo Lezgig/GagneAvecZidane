3cc0983b2c1a43d256f67fe5c86a2cb5e5d2a707
Eliot <gaillegue.eliot@gmail.com>
2023-01-27 11:31:11 +0000

    adding flex

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<h1> Site de Eliot Gaillegue </h1>
+<div style="display: flex">
+    <h1> Site de Eliot Gaillegue </h1>
 
-<?php echo "hello en php" ?>
+    <?php echo "hello en php" ?>
 
-<img src="https://idata.over-blog.com/2/60/21/58/mes-images-8/zidane-pub-98-leader-price.jpg" />
+    <img src="https://idata.over-blog.com/2/60/21/58/mes-images-8/zidane-pub-98-leader-price.jpg" />
+</div>
